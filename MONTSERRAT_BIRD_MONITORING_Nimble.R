318a002b998a47d266c0b4912921be64377c9348
Steffen Oppel <steffen.oppel@gmail.com>
2024-09-27 16:41:41 +0200

    started re-coding to Nimble

--- MONTSERRAT_BIRD_MONITORING_Nimble.R
+++ MONTSERRAT_BIRD_MONITORING_Nimble.R
@@ -4,35 +4,25 @@
 #############  steffen.oppel@gmail.com      ##########################################
 ######################################################################################
 
-### created 13 April 2014
 ### based on multi-year trend model of Kery et al. 2009
-### code adapted from AQWA monitoring of Oppel et al. 2014
 ### REQUIRES DATA PREPARATION FROM "MONTSERRAT_BIRD_MONITORING_data_prep.R"
-### last update 2 May 2014: include 'rain' (rainfall in previous 24 hrs at nearest rain station) as obsCovariate instead of 'Day'
-### updated to 2016 data on 24 May 2016 (without rainfall data but with simple 0/1 rain variable)
-
-### modified 13 April 2017 to fix figures for paper
-
-### modified 24 Dec 2017 to incorporate suggestion by David Borchers to include activity as observation-level covariate
-
-### modified 10 sEPT 2021 to GENERALISE output for multiple years
-
-### updated in 2024 to use Nimble
+### updated in 2024 to use Nimble and parallelize loop over species
 
 ######################################################################################
 #############  Load required packages       ##########################################
 ######################################################################################
 
 library(tidyverse)
-library(reshape)
-library(R2jags)
-library(jagsUI)
-#install.packages("rjags")
-library(rjags)
+library(dtplyr)
+library(lubridate)
 library(ggplot2)
 library(knitr)
 library(rmarkdown)
+library(MCMCvis)
 library(nimble)
+library(basicMCMCplots) # for trace plots called chainsPlot
+library(parallel)
+
 
 ######################################################################################
 #############  Set your working directory (path where the database is)       #########
@@ -165,6 +155,188 @@ for (d in 1:nyears){							### replace missing dates with mean for each survey r
   wind[is.na(wind[,2,d]),2,d]<-0
   wind[is.na(wind[,3,d]),3,d]<-0
 }
+
+
+
+
+
+
+######################################################################################
+#############  WRITE THE NIMBLE MODEL AND SET INITS  ############################
+######################################################################################
+
+# Specify model in NIMBLE format
+trend.model<-nimbleCode({
+  
+  
+  ####  Priors ########
+  loglam~dunif(-5,5)          ##  mean abundance prior
+  trend~dunif(-10,10)         ##  trend prior
+  beta.elev~dunif(-5,5)
+  beta.canopy~dunif(-5,5)
+  beta.treeheight~dunif(-5,5)
+  bwind~dunif(-5,5)
+  btime~dunif(-5,5)
+  bridge~dunif(-5,5)
+  bact~dunif(-5,5)
+  
+  ## SITE RANDOM EFFECT ##
+  for(i in 1:nsite){
+    lam.site[i]~dnorm(loglam,tau.site)    ## site-specific random effect with hierarchical centering from Kery email 5 June 2018
+  }
+  tau.site<-1/(sigma.site*sigma.site)
+  sigma.site~dunif(0,10)
+  
+  ## YEAR RANDOM EFFECT FOR ABUNDANCE AND ANNUALLY VARYING DETECTION PROBABILITY ##
+  for(year in 1:nyear){
+    p0[year]~dunif(0,1)## detection probability
+    logitp0[year]<-log(p0[year]/(1-p0[year]))
+    lam.year[year]~dnorm(trend*primocc[year],tau.year)    ## year-specific random effect with hierarchical centering from Kery email 5 June 2018
+  }
+  tau.lp<-1/(sigma.p*sigma.p)
+  sigma.p~dunif(0,10)
+  tau.year<-1/(sigma.year*sigma.year)
+  sigma.year~dunif(0,10)
+  
+  
+  ######### State and observation models ##############
+  for(year in 1:nyear){
+    for(i in 1:nsite){
+      log(lambda[i,year])<- lam.year[year]+beta.elev*elev[i,year]+beta.treeheight*treeheight[i,year]+beta.canopy*canopy[i,year]+lam.site[i]
+      N[i,year]~dpois(lambda[i,year])
+      
+      for(t in 1:nrep){
+        M[i,t,year]~dbin(p[i,t,year],N[i,year])
+        p[i,t,year] <- exp(lp[i,t,year])/(1+exp(lp[i,t,year]))
+        lp[i,t,year] ~ dnorm(mu.lp[i,t,year], tau.lp)
+        mu.lp[i,t,year]<-logitp0[year] + btime*time[i,t,year]+ bridge*ridge[i,t,year]+ bwind*wind[i,t,year]+ bact*ACT[i,t,year]
+        
+      }
+    }
+    
+    ### DERIVED PARAMETER FOR EACH YEAR ###
+    totalN[year]<-sum(N[,year])
+    anndet[year]<-mean(p[,,year])
+  }
+  
+  # Computation of fit statistic (Bayesian p-value)
+  # Fit statistic for observed data
+  # Also, generate replicate data and compute fit stats for them
+  for(year in 1:nyear){
+    for(i in 1:nsite){
+      for(t in 1:nrep){
+        
+        # Actual data
+        eval[i,t,year] <-N[i,year]*p[i,t,year] # Expected value
+        sd.resi[i,t,year]<-sqrt(eval[i,t,year]*(1-p[i,t,year])) +0.5
+        E[i,t,year]<-(M[i,t,year]-eval[i,t,year])/ sd.resi[i,t,year]
+        E2[i,t,year] <- pow(E[i,t,year],2)
+        
+        # Replicate data sets
+        M.new[i,t,year]~dbin(p[i,t,year],N[i,year])
+        E.new[i,t,year]<-(M.new[i,t,year]-eval[i,t,year])/sd.resi[i,t,year]
+        E2.new[i,t,year] <- pow(E.new[i,t,year], 2)
+      }
+    }
+  }
+  fit <- sum(E2[,,])# Sum up squared residuals for actual data set
+  fit.new <- sum(E2.new[,,]) # Sum up for replicate data sets
+}) ## end of nimble code chunk
+
+
+
+# 5.1. Define initial values
+
+# Initial values for some parameters
+## MUST BE FOR ALL PARAMETERS
+## NIMBLE CAN HAVE CONVERGENCE PROBLEMS IF DIFFERENT INITS ARE SPECIFIED: https://groups.google.com/g/nimble-users/c/dgx9ajOniG8
+
+# Missing values (NAs) or non-finite values were found in model variables: fledglings
+inits.productivity <- list(intercept.succ = rnorm(1,0, 0.01),
+                           #rain.succ = rnorm(1,0, 1),
+                           #elevation.succ = rnorm(1,0, 1),
+                           mig.succ = rnorm(1,0, 1),
+                           age.succ = rnorm(1,0, 1),
+                           agemig.succ = rnorm(1,0, 1),
+                           succ.year = rnorm(length(unique(REKI.fec$year)),0, 1),
+                           fec.year = rnorm(length(unique(REKI.fec$year)),0, 1),
+                           succ.nest = rnorm(length(unique(REKI.fec$year_id)),0, 1),
+                           intercept.fec1 = rnorm(1,0, 0.01),
+                           #intercept.fec2 = rnorm(1,0, 0.01),
+                           intercept.fec3 = rnorm(1,0, 0.01),
+                           feed.fec = rnorm(1,0, 1),
+                           rain.fec = rnorm(1,0, 1),
+                           rodent.fec = rnorm(1,0, 1),
+                           prob=ifelse(flmat>0,1,0),
+                           #mig.fec = rnorm(1,0, 1),
+                           age.fec = rnorm(1,0, 1),
+                           sigma.nest= runif(1,0,2),
+                           sigma.year= runif(1,0,5)
+                           
+)
+
+
+#### DISTINGUISH CONSTANTS AND DATA----
+# Constants are values that do not change, e.g. vectors of known index values or the indices used to define for loops
+# Data are values that you might want to change, basically anything that only appears on the left of a ~
+productivity.constants <- list(AGE=as.numeric(scale(REKI.fec.nest$age_cy)), # also tried withou scaling and it makes no difference at all
+                               #ALT=as.numeric(scale(REKI.fec.nest$ALT)),
+                               #RAIN=as.numeric(scale(REKI.fec.nest$rain_days)),
+                               RAIN_SPELL=as.numeric(scale(REKI.fec.nest$rain_spell)),
+                               MIG=ifelse(is.na(REKI.fec.nest$MIG_PRED),0.5,REKI.fec.nest$MIG_PRED),
+                               FEED=ifelse(is.na(REKI.fec.nest$FEED),0,as.numeric(scale(REKI.fec.nest$FEED))),
+                               PREY=as.numeric(scale(REKI.fec.nest$rodent_median)),   ### alternatively tried med and het but gave instant invalid parent error for Z
+                               year=REKI.fec.nest$year-min(REKI.fec.nest$year)+1,
+                               nyears=length(unique(REKI.fec.nest$year)),
+                               nestID=as.numeric(as.factor(REKI.fec$year_id)),
+                               nnests=length(unique(as.numeric(as.factor(REKI.fec$year_id)))),
+                               N=dim(REKI.fec.nest)[1])
+
+
+productivity.data <- list(fledglings=ifelse(flmat>0,1,0))
+
+# 5.3. Define parameters to be monitored
+parameters.productivity <- c("intercept.succ","mig.succ","age.succ","agemig.succ","succ.year",
+                             "intercept.fec1","intercept.fec3","rodent.fec","feed.fec","rain.fec","age.fec")  ##"mig.fec", "elevation.succ","rain.succ",
+
+
+# MCMC settings
+# number of posterior samples per chain is n.iter - n.burnin
+n.iter <- 50000
+n.burnin <- 25000
+n.chains <- 4
+
+
+# PRELIMINARY TEST OF NIMBLE MODEL TO IDENTIFY PROBLEMS --------------------
+test <- nimbleModel(code = prod.model,
+                    constants=productivity.constants,
+                    data = productivity.data,
+                    inits = inits.productivity,
+                    calculate=TRUE)
+
+### make sure that none of the logProbs result in NA or -Inf as the model will not converge
+test$calculate()
+# test$calculate(nodes=c("intercept.nest","age.nest","mig.nest","agemig.nest","feed.nest",
+#                        "intercept.succ","elevation.succ","rain.succ","mig.succ","agemig.succ",
+#                        "intercept.fec1","intercept.fec1","intercept.fec3","rain.fec","feed.fec","rodent.fec","age.fec"))  # will sum all log probs - if there is -Inf or NA then something is not properly initialised
+# test$calculate(nodes=c("prob","fl0","fl1","fl2","fl3","psi","z"))  # check the model parameters that are internally estimated
+# test$calculate(nodes=c("tau.year","sigma.year","succ.year"))  # check the random effects parameters
+# test$calculate(nodes="fledglings") # this causes a NaN when the nest probability (z[i] = 0)
+# test$logProb_fledglings
+
+test$initializeInfo()
+#help(modelInitialization)
+
+### make sure that none of the logProbs result in NA or -Inf as the model will not converge
+configureMCMC(test) # check that the samplers used are ok - all RW samplers need proper inits
+
+
+  
+
+
+
+
+
 
 
 
